6f79ffdb2e7a7ceeba671c06ee3bbaa8cfd52d2c
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-10-09 11:38:18 +0200

    Completion of EX-08'Curl and define'

---
+++ exercise-8/solution.js
@@ -0,0 +1,7 @@
+// inside terminal -> type: curl https://jsonplaceholder.typicode.com/posts/1/comments -v  
+
+function solution(){
+    console.log('content-type: application/json; charset=utf-8');
+}
+
+solution();
